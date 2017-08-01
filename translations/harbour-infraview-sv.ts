afbf4a67c14ee1cd2ea08804f8452ec0d26e9c45
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-08-01 19:59:32 +0200

    Create harbour-infraview-sv.ts
    Here's the Swedish translation.

---
+++ translations/harbour-infraview-sv.ts
@@ -0,0 +1,321 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="24"/>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="44"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="49"/>
+        <source>Device network information</source>
+        <translation>Enhetens nätverksinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="58"/>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="76"/>
+        <source>Using </source>
+        <translation>Med hjälp av </translation>
+    </message>
+</context>
+<context>
+    <name>Arp</name>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="43"/>
+        <source>Loading ARP cache</source>
+        <translation>Läser in ARP-cache</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="69"/>
+        <source>ARP cache</source>
+        <translation>ARP-cache</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="127"/>
+        <source>Copy IP address to clipboard</source>
+        <translation>Kopiera IP-adress till urklipp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="133"/>
+        <source>Copy MAC address to clipboard</source>
+        <translation>Kopiera MAC-adress till urklipp</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceInfo</name>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="58"/>
+        <source>Retrieving device info</source>
+        <translation>Hämtar enhetsinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="89"/>
+        <source>Device info</source>
+        <translation>Enhetsinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="97"/>
+        <source>IP address</source>
+        <translation>IP-adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="120"/>
+        <source>MAC address</source>
+        <translation>MAC-adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="143"/>
+        <source>Hostname</source>
+        <translation>Värdnamn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="166"/>
+        <source>Vendor</source>
+        <translation>Leverantör</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="189"/>
+        <source>OS Family</source>
+        <translation>OS-familj</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="212"/>
+        <source>OS Name</source>
+        <translation>OS-namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="235"/>
+        <source>Last boot</source>
+        <translation>Senaste start</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="258"/>
+        <source>Uptime</source>
+        <translation>Upptid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="281"/>
+        <source>Open TCP ports</source>
+        <translation>Öppna TCP-portar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="291"/>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceList</name>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="42"/>
+        <source>Scanning for devices</source>
+        <translation>Söker efter enheter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="68"/>
+        <source>Devices</source>
+        <translation>Enheter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="135"/>
+        <source>Show more info</source>
+        <translation>Visa mer information</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="144"/>
+        <source>Copy IP address to clipboard</source>
+        <translation>Kopiera IP-adress till urklipp</translation>
+    </message>
+</context>
+<context>
+    <name>EffectsPage</name>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="228"/>
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LocationInfo</name>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="64"/>
+        <source>Retrieving IP location info</source>
+        <translation>Hämtar platsinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="95"/>
+        <source>IP location info</source>
+        <translation>IP-adressens platsinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="112"/>
+        <source>External IP</source>
+        <translation>Extern IP-adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="135"/>
+        <source>Timezone</source>
+        <translation>Tidszon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="158"/>
+        <source>Country</source>
+        <translation>Land</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="181"/>
+        <source>City</source>
+        <translation>Stad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="204"/>
+        <source>ISP</source>
+        <translation>ISP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="227"/>
+        <source>rDNS</source>
+        <translation>rDNS</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="40"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="58"/>
+        <source>External IP info</source>
+        <translation>Extern IP-information</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="64"/>
+        <source>Devices in current network</source>
+        <translation>Enheter i aktuellt nätverk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="70"/>
+        <source>ARP cache table</source>
+        <translation>ARP cache-tabell</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <source>Network port connections</source>
+        <translation>Nätverkets portanslutningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="86"/>
+        <source>Network type</source>
+        <translation>Nätverkstyp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="107"/>
+        <source>Network device</source>
+        <translation>Nätverksenhet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="149"/>
+        <source>Gateway</source>
+        <translation>Gateway</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="170"/>
+        <source>IP address</source>
+        <translation>IP-adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="128"/>
+        <source>Subnet Mask</source>
+        <translation>Subnätmask</translation>
+    </message>
+</context>
+<context>
+    <name>Netstat</name>
+    <message>
+        <location filename="../qml/pages/Netstat.qml" line="50"/>
+        <source>Collecting data...</source>
+        <translation>Samlar in data...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Netstat.qml" line="105"/>
+        <source>Local connections</source>
+        <translation>Lokala anslutningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Netstat.qml" line="161"/>
+        <source>Show more info</source>
+        <translation>Visa mer information</translation>
+    </message>
+</context>
+<context>
+    <name>NetstatInfo</name>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="37"/>
+        <source>Connection info</source>
+        <translation>Anslutningsinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="45"/>
+        <source>Proto</source>
+        <translation>Protokoll</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="68"/>
+        <source>User</source>
+        <translation>Användare</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="91"/>
+        <source>Program name</source>
+        <translation>Programnamn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="114"/>
+        <source>Process ID</source>
+        <translation>Process-ID</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="135"/>
+        <source>Local address</source>
+        <translation>Lokal adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="158"/>
+        <source>Local port</source>
+        <translation>Lokal port</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="181"/>
+        <source>Foreign address</source>
+        <translation>Fjärradress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="204"/>
+        <source>Foreign port</source>
+        <translation>Fjärrport</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="227"/>
+        <source>State</source>
+        <translation>Status</translation>
+    </message>
+</context>
+</TS>
