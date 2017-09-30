e452617385a17c160f5c4c523f1411ba1d473f6b
arno <adekker@gmail.com>
2017-09-30 15:15:18 +0200

    Merged PR RobbBienert

--- src/infraview-helper/infraview-helper.c
+++ src/infraview-helper/infraview-helper.c
@@ -4,6 +4,26 @@
 #include <string.h>
 
 #define DELIM "."
+const char DEL_ARP[] = "delete_arp";
+const char NETSTAT_PY[] = "/usr/share/harbour-infraview/python/netstat.py";
+char *IP_FLUSH_APP[] = {
+        "/sbin/ip", "-s", "-s", "neigh", "flush", "all", NULL
+        };
+
+/**
+ * wraps the call to execvp including perror output
+ *
+ * @param args  NULL terminated array of C strings to execute
+ *
+ * This function wraps the call to execvp and the error handling.
+ * The first item in args is used as command to execute, so it is passed
+ * as first argument to execvp. The whole array args is then passes as
+ * second argument to execvp.
+ */
+void execa(char * const args[]) {
+    execvp(*args, args);
+    perror(*args);
+}
 
 /* return 1 if string contain only digits, else return 0 */
 int valid_digit(char *ip_str)
@@ -58,8 +78,10 @@ int is_valid_ip(char *ip_str)
 }
 
 int main(int argc, char **argv) {
-    char command[4096] = "";
-    char cmdline[4096];
+    char command[20] = "";
+    int i;      /* loop index */
+    char **arpcmd;  /* complete arp command with arguments */
+
     if (setuid(0) != 0) {
         perror("Setuid failed, no suid-bit set?");
         return 1;
@@ -71,16 +93,16 @@ int main(int argc, char **argv) {
         perror("Too many arguments");
         return 1;
     }
+    /* no command line argument: call netstat.py */
     if (argc == 1) {
-        int netstatCall = system("/usr/share/harbour-infraview/python/netstat.py");
-        if (netstatCall == -1) {
-            perror("Could not execute netstat.py");
-        }
-    } else if (strcmp(command, "delete_arp") == 0) {
-        int flushCall = system("/sbin/ip -s -s neigh flush all");
-        if (flushCall == -1) {
-            perror("Could not execute arpflush");
-        }
+        execlp(NETSTAT_PY, NETSTAT_PY, (char*)NULL);
+        perror(NETSTAT_PY);
+    }
+    /* 1 certain command line argument */
+    else if (argc == 2 &&
+        strncmp(argv[1], DEL_ARP, sizeof(DEL_ARP)) == 0)
+    {
+        execa(IP_FLUSH_APP);
     } else {
         char ip[16];
         strcpy (ip, command);
@@ -90,11 +112,30 @@ int main(int argc, char **argv) {
             printf("Invalid IP\n");
             return 1;
         }
-        sprintf(cmdline, "/sbin/arp -d %s", command);
-        int arpDel = system(cmdline);
-        if (arpDel == -1) {
-            perror("Could not clear arp entry");
+        /* We have to allocate a new array as we extend argv by
+         * two more items. This array holds enough pointers (!)
+         * for the strings in argv + our two strings.
+         */
+        if ((arpcmd = (char**) calloc(argc+2, sizeof(char*))) == NULL)
+        {
+            perror("calloc failed");
+            return 1;
         }
+
+        arpcmd[0] = "/sbin/arp";
+        arpcmd[1] = "-d";
+
+        /* We can do here a cheap copy by just let the array items
+         * point to the items in argv. If argv is going to be
+         * changed you should do a real copy with strdup.
+         */
+        for (i = 1; i <= argc; ++i) {
+            arpcmd[i+1] = argv[i];
+        }
+
+        execa(arpcmd);
+        /* If the exec fails we can be so kind an free our memory. */
+        free(arpcmd);
     }
-    return 0;
+    return 1;
 }
