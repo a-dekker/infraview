e0559df083ea7e093e029b929bfe06d3102a78b3
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2018-02-13 20:54:57 +0100

    Added translation to Spanish language

---
+++ translations/harbour-infraview-es.ts
@@ -0,0 +1,401 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="24"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="44"/>
+        <source>Version</source>
+        <translation>Versión</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="49"/>
+        <source>Device network information</source>
+        <translation>Información de la red de dispositivos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="58"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="76"/>
+        <source>Using </source>
+        <translation>Usa </translation>
+    </message>
+</context>
+<context>
+    <name>Arp</name>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="56"/>
+        <source>Loading ARP cache</source>
+        <translation>Cargando caché ARP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="81"/>
+        <source>Clear all ARP entries</source>
+        <translation>Limpiar entradas ARP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="82"/>
+        <source>Clearing</source>
+        <translation>Limpiando</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="96"/>
+        <source>ARP cache</source>
+        <translation>Caché ARP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="134"/>
+        <source>Removing </source>
+        <translation>Eliminando </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="163"/>
+        <source>Copy IP address to clipboard</source>
+        <translation>Copiar dirección IP al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="169"/>
+        <source>Copy MAC address to clipboard</source>
+        <translation>Copiar dirección MAC al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Arp.qml" line="175"/>
+        <source>Remove ARP entry</source>
+        <translation>Eliminar entrada ARP</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="36"/>
+        <source>Network name</source>
+        <translation>Nombre de la red</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="47"/>
+        <source>Subnet Mask</source>
+        <translation>Máscara de subred</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="58"/>
+        <source>IP address</source>
+        <translation>Dirección IP</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="25"/>
+        <source>Network type</source>
+        <translation>Tipo de red</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceInfo</name>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="58"/>
+        <source>Retrieving device info</source>
+        <translation>Recuperando info del dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="89"/>
+        <source>Device info</source>
+        <translation>Info del dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="97"/>
+        <source>IP address</source>
+        <translation>Dirección IP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="120"/>
+        <source>MAC address</source>
+        <translation>Dirección MAC</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="143"/>
+        <source>Hostname</source>
+        <translation>Nombre de host</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="166"/>
+        <source>Vendor</source>
+        <translation>Proveedor</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="189"/>
+        <source>OS Family</source>
+        <translation>Familia del SO</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="212"/>
+        <source>OS Name</source>
+        <translation>Nombre del SO</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="235"/>
+        <source>Last boot</source>
+        <translation>Último inicio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="258"/>
+        <source>Uptime</source>
+        <translation>Tiempo de actividad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="281"/>
+        <source>Open TCP ports</source>
+        <translation>Puertos TCP abiertos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceInfo.qml" line="291"/>
+        <source>None</source>
+        <translation>Ninguno</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceList</name>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="42"/>
+        <source>Scanning for devices</source>
+        <translation>Escaneando dispositivos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="68"/>
+        <source>Devices</source>
+        <translation>Dispositivos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="135"/>
+        <source>Show more info</source>
+        <translation>Mostrar más info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DeviceList.qml" line="144"/>
+        <source>Copy IP address to clipboard</source>
+        <translation>Copiar dirección IP al portapapeles</translation>
+    </message>
+</context>
+<context>
+    <name>EffectsPage</name>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="228"/>
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LocationInfo</name>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="64"/>
+        <source>Retrieving IP location info</source>
+        <translation>Recuperando info de la IP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="95"/>
+        <source>IP location info</source>
+        <translation>Info de ubicación de la IP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="112"/>
+        <source>External IP</source>
+        <translation>IP pública</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="135"/>
+        <source>Timezone</source>
+        <translation>Zona horaria</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="158"/>
+        <source>Country</source>
+        <translation>País</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="181"/>
+        <source>City</source>
+        <translation>Ciudad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="204"/>
+        <source>ISP</source>
+        <translation>ISP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationInfo.qml" line="227"/>
+        <source>rDNS</source>
+        <translation>rDNS</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="50"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="54"/>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="79"/>
+        <source>External IP info</source>
+        <translation>Info de IP pública</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="84"/>
+        <location filename="../qml/pages/MainPage.qml" line="92"/>
+        <source>Devices in current network</source>
+        <translation>Dispositivos en red actual</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="101"/>
+        <source>ARP cache table</source>
+        <translation>Tabla de caché ARP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="106"/>
+        <location filename="../qml/pages/MainPage.qml" line="114"/>
+        <source>Network port connections</source>
+        <translation>Conexiones de puertos de red</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="125"/>
+        <source>Network type</source>
+        <translation>Tipo de red</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="146"/>
+        <source>Network name</source>
+        <translation>Nombre de red</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="167"/>
+        <source>Network device</source>
+        <translation>Nombre de dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="188"/>
+        <source>Subnet Mask</source>
+        <translation>Máscara de subred</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="209"/>
+        <source>Gateway</source>
+        <translation>Puerta de enlace</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="231"/>
+        <source>Broadcast</source>
+        <translation>Broadcast</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="253"/>
+        <source>Domain</source>
+        <translation>Dominio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="275"/>
+        <source>DHCP</source>
+        <translation>DHCP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="297"/>
+        <source>DNS</source>
+        <translation>DNS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="318"/>
+        <source>IP address</source>
+        <translation>Dirección IP</translation>
+    </message>
+</context>
+<context>
+    <name>Netstat</name>
+    <message>
+        <location filename="../qml/pages/Netstat.qml" line="55"/>
+        <source>Collecting data...</source>
+        <translation>Recopilando datos...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Netstat.qml" line="110"/>
+        <source>Local connections</source>
+        <translation>Conexiones locales</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Netstat.qml" line="167"/>
+        <source>Show more info</source>
+        <translation>Mostrar más info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Netstat.qml" line="184"/>
+        <source>Kill process</source>
+        <translation>Matar proceso</translation>
+    </message>
+</context>
+<context>
+    <name>NetstatInfo</name>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="37"/>
+        <source>Connection info</source>
+        <translation>Info de la conexión</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="45"/>
+        <source>Proto</source>
+        <translation>Proto</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="68"/>
+        <source>User</source>
+        <translation>Usuario</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="91"/>
+        <source>Program name</source>
+        <translation>Nombre del programa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="114"/>
+        <source>Process ID</source>
+        <translation>ID del proceso</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="135"/>
+        <source>Local address</source>
+        <translation>Dirección local</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="158"/>
+        <source>Local port</source>
+        <translation>Puerto local</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="181"/>
+        <source>Foreign address</source>
+        <translation>Dirección remota</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="204"/>
+        <source>Foreign port</source>
+        <translation>Puerto remoto</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NetstatInfo.qml" line="227"/>
+        <source>State</source>
+        <translation>Estado</translation>
+    </message>
+</context>
+</TS>
